4ab029f2c1283b2700ea7214d27c4758b1add7d8
John Bowser <john.bow7720@gmail.com>
2020-08-11 09:49:33 -0600

    first go at sort functionality.

--- playstoreServer.js
+++ playstoreServer.js
@@ -1,7 +1,36 @@
 const express = require('express');
 const morgan = require('morgan');
-const storeData = require('playstore-data.js');
+const storeData = require('./playstore-data.js');
 
 const app = express();
 
-app.use(morgan());
+app.use(morgan('dev'));
+
+
+
+app.get('/apps', (req, res) => {
+  const { sort, genres } = req.query;
+
+  const validSort = ['Rating', 'App'];
+  
+  if(sort && !validSort.includes(sort)) {
+    return res.status(400).send('Please sort by either rating or app');
+  }
+
+  let sortedData = [];
+  if(sort) {
+    sortedData = storeData.sort((a, b) => {
+      if(a[sort] < b[sort]) {
+        return -1;
+      } else if(a[sort] > b[sort]) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  }
+
+  res.json(sortedData);
+});
+
+app.listen(8000, () => console.log('Listening on port 8000'));
