e393dacbaaedca47e498ff94ba3d0c643a45f522
Michael Oldacre <mloldacre@live.com>
2020-08-11 12:17:58 -0400

    Sort functionality complete, genre filter added

--- playstoreServer.js
+++ playstoreServer.js
@@ -12,17 +12,43 @@ app.get('/apps', (req, res) => {
   const { sort, genres } = req.query;
 
   const validSort = ['Rating', 'App'];
+  const validGenre = ['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'];
+
+  //Filtering by genre
+  if (genres && !validGenre.includes(genres)) {
+    return res.status(400).send('Please filter by "Action, Puzzle, Strategy, Casual, Arcade or Card"');
+  }
+
+  let filterData = [...storeData];
   
-  if(sort && !validSort.includes(sort)) {
+  if (genres) {
+    filterData = filterData.filter(app =>
+      app.Genres.includes(genres));
+  }
+
+
+  //Sorting
+  if (sort && !validSort.includes(sort)) {
     return res.status(400).send('Please sort by either rating or app');
   }
 
-  let sortedData = [];
-  if(sort) {
-    sortedData = storeData.sort((a, b) => {
-      if(a[sort] < b[sort]) {
+  if (sort &&  sort === 'Rating') {
+    filterData = filterData.sort((a, b) => {
+      if (a[sort] > b[sort]) {
         return -1;
-      } else if(a[sort] > b[sort]) {
+      } else if (a[sort] < b[sort]) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  }
+  
+  if (sort && sort === 'App') {
+    filterData = filterData.sort((a, b) => {
+      if (a[sort].toLowerCase() < b[sort].toLowerCase()) {
+        return -1;
+      } else if (a[sort].toLowerCase() > b[sort].toLowerCase()) {
         return 1;
       } else {
         return 0;
@@ -30,7 +56,7 @@ app.get('/apps', (req, res) => {
     });
   }
 
-  res.json(sortedData);
+  res.json(filterData);
 });
 
 app.listen(8000, () => console.log('Listening on port 8000'));
